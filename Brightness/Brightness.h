f8950683907381cc5b2e9ee95d09c20402784d0c
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 08:00:21 +0100

    Initial Commit

---
+++ Brightness/Brightness.h
@@ -0,0 +1,19 @@
+//
+//  Brightness.h
+//  Brightness
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Brightness.
+FOUNDATION_EXPORT double BrightnessVersionNumber;
+
+//! Project version string for Brightness.
+FOUNDATION_EXPORT const unsigned char BrightnessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Brightness/PublicHeader.h>
+
+
